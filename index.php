158e211b9c09f37f116d3f292f89b3b4598cec94
김재홍 <jjaekong@gmail.com>
2016-04-25 19:47:47 +0900

    메인 레이아웃 작업 중...

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>백제예술대학교</title>
+    <link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/default.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <header id="header">
+        <div class="container">
+            <h1><a href="#"><img src="/images/logo.png" alt="백제예술대학교"></a></h1>
+            <nav class="util">
+                <ul>
+                    <li><a href="#">취업정보</a></li>
+                    <li><a href="#">기숙사</a></li>
+                    <li><a href="#">도서관</a></li>
+                    <li><a href="#">통학버스</a></li>
+                    <li><a href="#">교직원</a></li>
+                </ul>
+                <div class="campus dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="campus-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        교내사이트
+                    </button>
+                    <button type="button" class="btn btn-default dropdown-toggle btn-caret" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">목록 열기/닫기</span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="campus-dropdown">
+                        <li><a href="#">교내사이트 #1</a></li>
+                        <li><a href="#">교내사이트 #2</a></li>
+                        <li><a href="#">교내사이트 #3</a></li>
+                        <li><a href="#">교내사이트 #4</a></li>
+                    </ul>
+                </div>
+                <div class="depart dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="depart-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        학과사이트
+                    </button>
+                    <button type="button" class="btn btn-default dropdown-toggle btn-caret" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">목록 열기/닫기</span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="depart-dropdown">
+                        <li><a href="#">학과사이트 #1</a></li>
+                        <li><a href="#">학과사이트 #2</a></li>
+                        <li><a href="#">학과사이트 #3</a></li>
+                        <li><a href="#">학과사이트 #4</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <nav class="gnb">
+            <ul>
+                <li>
+                    <a href="#">대학소개</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">학과안내</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">입학안내</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">대학생활</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">백제광장</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">교수학습지원센터</a>
+                    <div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">NCS지원센터</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <main id="content">
+    </main>
+    <footer id="footer">
+        <div class="container">
+            <h2><img src="/images/logo_white.png" alt="백제예술대학교"></h2>
+            <nav class="policy">
+                <ul>
+                    <li><a href="#">공지</a></li>
+                    <li><a href="#">예결산공고</a></li>
+                    <li><a href="#">입찰공고</a></li>
+                    <li><a href="#">대학정보공사</a></li>
+                    <li><a href="#">정보공개</a></li>
+                    <li><a href="#">대학평의원회</a></li>
+                    <li><a href="#">대학자체평가</a></li>
+                    <li><a href="#">전북문화예술교육원</a></li>
+                    <li><a href="#">개인정보취급방침</a></li>
+                </ul>
+            </nav>
+            <div class="contact">
+                <ul>
+                    <li><address>565-702 전북 완주군 봉동읍 백제대학로 171</address></li>
+                    <li>
+                        <dl>
+                            <dt>TEL</dt>
+                            <dd>063.260.9114</dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>FAX</dt>
+                            <dd>063.263.3607</dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>입시문의  TEL</dt>
+                            <dd>063.260.9001</dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>FAX</dt>
+                            <dd>063.260.9006</dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <div class="copyright">
+                <p>© 2016 PAEKCHE INSTITUTE OF THE ARTS. ALL RIGHTS RESERVED. SITE BY <a href="http://movementk.com" target="_blank">MOVEMENT K GROUP</a>.</p>
+            </div>
+            <div class="commission">
+                <p><img src="/images/img_youth_commission.png" alt="대통령직속 청년위원회"></p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
