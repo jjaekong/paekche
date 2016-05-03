d2e351fd3167e00e75d2a89ffb8a255fbc9294c4
kyh5392 <kyh90495392@gmail.com>
2016-05-03 10:00:52 +0900

    대학소개 > 설립이념 작업 완료

---
+++ greeting.php
@@ -0,0 +1,111 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/css/introduce.css" rel="stylesheet" />
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <div class="wrapper">
+        <div class="visual visual-1">
+            <div class="container">
+                <p>
+                    CREATE THE UNCREATED<br>
+                    PAEKCHE INSTITUTE OF THE ARTS
+                </p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="aside">
+                <aside id="aside">
+                    <h3>대학소개</h3>
+                    <nav id="lnb">
+                        <ul>
+                            <li><a href="#">설립이념</a></li>
+                            <li class="active"><a href="#">이사장 인사말</a></li>
+                            <li><a href="#">총장 인사말</a></li>
+                            <li><a href="#">대학연혁</a></li>
+                            <li><a href="#">비전 및 발전전략</a></li>
+                            <li><a href="#">특성화전략</a></li>
+                            <li><a href="#">상징소개</a></li>
+                            <li><a href="#">학교조직</a></li>
+                            <li><a href="#">찾아오시는 길</a></li>
+                            <li><a href="#">캠퍼스 안내</a></li>
+                        </ul>
+                    </nav>
+                    <nav class="quick">
+                        <ul class="row">
+                            <li class="col-xs-6"><a href="#">인터넷 증명발급</a></li>
+                            <li class="col-xs-6"><a href="#">재학생 서비스</a></li>
+                            <li class="col-xs-6"><a href="#">찾아오시는 길</a></li>
+                            <li class="col-xs-6"><a href="#">행정정보시스템</a></li>
+                        </ul>
+                    </nav>
+                </aside>
+            </div>
+            <div class="content">
+                <ol class="breadcrumb">
+                    <li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+                    <li><a href="#">대학소개</a></li>
+                    <li class="active">설립이념</li>
+                </ol>
+                <main id="content" class="introduce vision">
+					<div class="page-header">
+                        <h2>설립이념<small>우리 대학의 설립과 <i>교육 목표 체계</i></small></h2>
+                        <hr>
+                    </div>
+
+					<section class="ideology">
+						<div class="section-header">
+							<h3 class="section-title">건학이념</h3>
+						</div>
+						<div class="section-content">
+							<p>
+								우리문화예술의 정통성을 바탕으로 그 본류를 주도하고 미래를 선도하는<br>
+								<strong>"예술인 교육"</strong>
+							</p>
+							<img src="/images/m_visual.jpg" alt="자연인,문화인,예술인">
+						</div>
+					</section>
+					<section class="purpose">
+						<div class="section-header">
+							<h3> 교육목적</h3>
+						</div>
+						<div class="section-content">
+							<p>
+								<strong>"우리문화예술의 정통성을 바탕으로 그 본류를 주도하고 미래를 선도하는 예술인 교육"</strong><br>
+								이라는 건학 이념을 구현하기 위해 우리는 우리문화예술의 본질을 이해하고 존중하며, 사회와 같이 호흡하고 고민하는 예술가 정신을 함양하여 각자의 예술표현의 욕구를 실제 예술현장에서 살아 있는 모습으로 행위 함으로써, 당대 문화예술의 본류를 이루고 또 이끌어가는 예술인, 예능인으로 교육하고자 한다.
+							</p>
+						</div>
+					</section>
+					<section class="target">
+						<div class="section-header">
+							<h3>교육목표</h3>
+						</div>
+						<p>우리 백제예술대학교는 다음과 같은 예술인상을 길러낸다.</p>
+						<div class="section-content">
+							<ul>
+								<li>
+									<img src="/images/cont_img1.jpg" alt="">
+									<p>사회와 대중과 같이<br>호흡하는 예술인</p>
+								</li>
+								<li>
+									<img src="/images/cont_img2.jpg" alt="">
+									<p>창의적인<br>예술인</p>
+								</li>
+								<li>
+									<img src="/images/cont_img3.jpg" alt="">
+									<p>행동하고 고민하는<br>예술인</p>
+								</li>
+								<li>
+									<img src="/images/cont_img4.jpg" alt="">
+									<p>시대적 요구와 조화되는<br>예술인</p>
+								</li>
+							</ul>
+						</div>
+					</section>
+                </main>
+            </div>
+        </div>
+    </div>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
