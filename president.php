881162710fe7139971c18961fc0fe34eec768a74
kyh5392 <kyh90495392@gmail.com>
2016-05-03 12:34:33 +0900

    대학소개 > 총장 인사말 작업 완료.

--- president.php
+++ president.php
@@ -19,8 +19,8 @@
                     <nav id="lnb">
                         <ul>
                             <li><a href="#">설립이념</a></li>
-                            <li class="active"><a href="#">이사장 인사말</a></li>
-                            <li><a href="#">총장 인사말</a></li>
+                            <li><a href="#">이사장 인사말</a></li>
+                            <li class="active"><a href="#">총장 인사말</a></li>
                             <li><a href="#">대학연혁</a></li>
                             <li><a href="#">비전 및 발전전략</a></li>
                             <li><a href="#">특성화전략</a></li>
@@ -44,43 +44,44 @@
                 <ol class="breadcrumb">
                     <li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
                     <li><a href="#">대학소개</a></li>
-                    <li class="active">이사장 인사말</li>
+                    <li class="active">총장 인사말</li>
                 </ol>
-                <main id="content" class="introduce chairman">
+                <main id="content" class="introduce president">
 					<div class="page-header">
-                        <h2>이사장 인사말</h2>
+                        <h2>총장 인사말</h2>
                         <hr>
                     </div>
-					<section class="chairman">
+					<section class="chancellor">
 						<div class="section-header">
-							<h3 class="section-title">문화 예술인의 터전,<span>백제예술대학교</span></h3>
+							<h3 class="section-title">함께 꿈꾸면<span>이루어집니다.</span></h3>
 						</div>
 						<div class="section-content">
 							<div class="introduce">	
-								<img src="/images/introduce/chairman.jpg " alt="이사장인사말">
+								<img src="/images/introduce/president.jpg " alt="총장인사말">
 								<ul>
-									<li>서울대학교 사회학과 졸업</li>
-									<li>전라북도지사</li>
-									<li>경향신문사 정치부장</li>
-									<li>MBC 문화방송 사장</li>
+									<li>서울대학교 공과대학 졸업</li>
+									<li>건국대 행정대학원 도시개발학과석사</li>
 								</ul>
 								<ul>
-									<li>(주) MBC경향신문 사장</li>
-									<li>백제문화 연구원 상임고문</li>
-									<li>전통문화 예술진흥회 총재</li>
-									<li>전주이씨 대동종악원 이사장</li>
-								</ul>	
+									<li>건국대 대학원 행정학과 박사</li>
+									<li>現 백제예술대학교 총장</li>
+								</ul>
 							</div>
 							<div class="intro">
 								<p>
-									인류의 문화는 예술이라는 표현수단을 통하여 전래, 발전되어 왔으며<br> 찬란한 문화유산과 역사를 자랑하는 우리민족은 심오하고 독특한<br>
-									예술적 감각을 지니고있습니다. 근래의 경제사회적 변혁과 양적인<br> 팽창시대의 우리 문화예술도끊임없는 신사조의 출현과 예술작품의 홍수<br>
-									속에 오늘을 맞이하고 있습니다. 
+									안녕하십니까.<br> 
+									우리 백제예술대학교를 찾아주신 모든 분들을 환영합니다.<br>
+									오늘날 우리사회는 근대화산업 개발의 시대, 정보통신산업의 시대를<br> 넘어서 문화산업의 팽창시대를 맞이하게 되었습니다.<br><br> 
+									이렇게 세계화된 문화산업 시대를 이끌어갈 참신한 인재를 육성해야 할<br> 문화예술 교육의 역할과 중요성도 커졌다고 생각합니다.<br>
+									우리 대학은 1992년 개교 이래 20여년의 성장을 거듭하여 이제는 안정<br>되고 효율적인 예술교육 시스템을 완비해 학생들을 교육하고 있습니다.
 								</p>
 								<p>
-									이에 우리 백제예술대학교는 전통성 있는 우리 문화의 본질을 이해하고<br> 표현하며 미래를 선도적으로 이끌어 갈 문화 예술인의 터전을 마련하여,<br> 자연인, 문화인,예술인의 교훈을 바탕으로 역사의 흐름 속에서<br> 한국문화의 본류와 함께 발전할 것입니다. 
+									우리 백제예술대학교는 이러한 예술교육의 중심체로서의 역할을<br> 다하고자 형식적인 틀에 구애 받지 않고열린 교육을 지향하는 현장 교육<br> 
+									중심의 특화된 교육방침을 계속 유지해 나갈 것입니다.<br><br>
+									21세기 문화예술의 시대의 본류를 같이 호흡하여 세계 속에 참된 우리<br> 예술문화의 주인이 되기 위하여 우리 백제예술대학교는 자신 있게<br>
+									끊임없이 노력할 것입니다. 
 								</p>
-								<img src="/images/introduce/chief.png" alt="학교법인 백암학원 이사장 이환희">
+								<img src="/images/introduce/chancellor.png" alt="백제예술대학교 총장 이기훈">
 							</div>
 						</div>
 					</section>
