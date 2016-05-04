bf89d0c6d9f58f284ac26e88be08566482d9c2c8
kyh5392 <kyh90495392@gmail.com>
2016-05-04 17:48:45 +0900

    대학소개 > 특성화전략 작업 완료.

--- character.php
+++ character.php
@@ -44,82 +44,32 @@
                 <ol class="breadcrumb">
                     <li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
                     <li><a href="#">대학소개</a></li>
-                    <li class="active">비전ㆍ발전전략</li>
+                    <li class="active">특성화전략</li>
                 </ol>
-                <main id="content" class="introduce strategic">
+                <main id="content" class="introduce character">
 					<div class="page-header">
-                        <h2>비전ㆍ발전전략</h2>
+                        <h2>특성화전략<small> 특성화목표와<i> 추진방향</i></small></h2>
+						<hr>
                     </div>
-					<section class="strategy">
-						<div class="vision">
-							<h3>비전</h3>
-							<p>현재를 주도하고 미래를 선도하는 예술대학</p>
+					<section class="artist">
+						<div class="future">
+							<h3>특성화</h3>
+							<h4><span>"현재"</span>를 주도하고 <span>"미래"</span>를 선도하는 예술대학</h4>
 						</div>
-						<div class="mission">
-							<h3>미션</h3>
-							<p>문화 예술 산업을 이끄는 전물예술 인재 육성</p>
-						</div>
-						<div class="target">
-							<h3>목표</h3>
-							<ul>
-								<li><p>사회와 대중과 같이<br>호흡하는 예술인</p></li>
-								<li><p>행동하고<br>고민하는 예술인</p></li>
-								<li><p>창의적인 예술인</p></li>
-								<li><p>시대적 요구와<br>조화되는 예술인</p></li>
-							</ul>
-						</div>
-						<div class="core">
-							<h3>핵심분야 및 핵심전략</h3>
-							<ol>
-								<li>
-									<dl>
-										<dt><h4>학생교육</h4></dt>
-										<dd>예술현장 중심의 맞춤형 교육</dd>
-										<dd>예술 수요자 중심의 교육 체제 구축</dd>
-										<dd>선진문화 국제교류(글로벌)</dd>
-									</dl>
-								</li>
-								<li>
-									<dl>
-										<dt><h4>연구강화(교원)</h4></dt>
-										<dd>연구성과 극대화</dd>
-										<dd>창의적인 예능교육 프로그램 연구</dd>
-									</dl>
-								</li>
-								<li>
-									<dl>
-										<dt><h4>산학협력</h4></dt>
-										<dd>밀착형 산학협력 강화</dd>
-										<dd>지역 예술봉사 지원 강화</dd>
-										<dd>취업강화</dd>
-									</dl>
-								</li>
-								<li>
-									<dl>
-										<dt><h4>특성화</h4></dt>
-										<dd>공연 • 문화 예술인 양성</dd>
-										<dd>글로벌 뮤지션 전문인력 양성</dd>
-										<dd>방송 • 영상 제작 인력</dd>
-									</dl>
-								</li>
-								<li>
-									<dl>
-										<dt><h4>경영의 합리화</h4></dt>
-										<dd>대학의 행정력 강화</dd>
-										<dd>교육환경 션 전문인력개선</dd>
-										<dd>재원의 다각화</dd>
-									</dl>
-								</li>
-							</ol>
-						<div>
-						<div class="task">
-							<p>중점 추진 과제 45개</p>
-						</div>
+						
 					</section>
                 </main>
             </div>
         </div>
     </div>
+<style>
+.introduce.character .artist .future {margin-top:50px; }
+.introduce.character .artist .future h3 {background:url("/images/introduce/m_img.png") center left no-repeat; text-indent:10px; }
+.introduce.character .artist .future h4 {margin-top:60px; font-size:26px; color:#555; letter-spacing:-1px; text-align:center; }
+.introduce.character .artist .future h4 span {color:#d22d36; }
+
+
+</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
 </body>
