3b458f1114f73fdf402c4cce69f30cd53043a9f4
kyh5392 <kyh90495392@gmail.com>
2016-06-08 16:47:12 +0900

    학과안내 > 학과안내, 교수진 완료 #1 , 학과안내 > 커뮤니티 , 학과활동갤러리 완료 #1

--- departments/introduce.php
+++ departments/introduce.php
@@ -1,5 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/dochead.php'); ?>
-<link href="/css/introduce.css" rel="stylesheet" />
+<link href="/departments/css/introduce.css" rel="stylesheet" />
 </head>
 <body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/header.php'); ?>
@@ -23,16 +23,16 @@
 	<main id="content" tabindex="-1" class="departments introduce">
 		<section class="intro">
 			<div class="container">
-				<div class="section-header header-background">
+				<div class="section-header">
 					<hr />
 					<h3>학과소개</h3>
 				</div>
 				<div class="section-content">
 					<h4>"K-POP을 이끌어 갈 뮤직엔터테이너"</h4>
 					<p>
-						한류를 이끌어온 <strong>강력한 영향력을 지닌 문화콘텐츠, K-POP의 미래 인재들을 양성하는데 목표</strong>를 두며,<br>퍼모먼스 주역인.....
 						한류를 이끌어온 <strong>강력한 영향력을 지닌 문화콘텐츠, K-POP의 미래 인재들을 양성하는데 목표</strong>를 두며,<br>
-						한류를 이끌어온 <strong>강력한 영향력을 지닌 문화콘텐츠, K-POP의 미래 인재들을 양성하는데 목표</strong>를 두며,<br>
+						퍼포먼스 주역인 ‘K-POP보컬’ 전공, 음악을 창작하는 ‘K-POP미디 작·편곡’ ,  ‘싱어송라이터’ 전공으로 나뉘며,<br>
+						각 전공별로 체계적이고 실제적 트레이닝을 위해 K-pop과 관련된 다양한 문화예술영역의 전문 교수진과 맞춤식으로 교육이 이루어지고 있다.
 					</p>
 					<p class="point">
 						학기별 기초-발전-심화과정을 통하여 <strong>세분화된 프로그램으로 교육하여 기본기를 갖춘 뮤직엔터테이너로서의 자질을 갖추도록 양성</strong>한다.
@@ -40,101 +40,82 @@
 					<div class="contact-office">
 						<dl>
 							<dt>학과사무실</dt>
-							<dd>063-260-9130</dd>
+							<dd><a href="tel:0632609130">063-260-9130</a></dd>
 							<dt>홈페이지</dt>
-							<dd>http://www.paekche.ac.kr/media</dd>
+							<dd><a href="http://www.paekche.ac.kr/media" target="_blank">http://www.paekche.ac.kr/media</a></dd>
 						</dl>
 					</div>
 				</div>
 			</div>
 		</section>
 		<div class="container">
-			<section class="test" style="height:500px;">
-				<div></div>
+			<section class="target">
+				<div class="section-header">
+					<hr />
+					<h3>교육목표</h3>
+				</div>
+				<div class="section-content">
+					<h4>
+						<small>미디어음악의 중심인 K-POP을 배경으로,</small>
+						<br>21세기를 이끌어 나갈 뮤직엔터테이너를 양성함
+					</h4>
+					<img src="/departments/images/target1.png" alt="음악산업을 이끌 창의적 예술인, 대중과 같이 호흡하는 K-POP 스타양성, 매력적인 뮤직엔터테이너">
+				</div>
+			</section>
+			<section class="path">
+				<div class="section-header">
+					<hr />
+					<h3>졸업 후 진로</h3>
+				</div>
+				<div class="section-content">
+					<ul class="content-list">
+						<li>대중음악가수</li>
+						<li>보컬세션</li>
+						<li>보컬트레이너</li>
+						<li>보컬코러스팀</li>
+						<li>보컬음향디렉터</li>
+						<li>음악 실기교사</li>
+						<li>음악 아카데미 운영</li>
+						<li>기획사 소속 트레이너</li>
+						<li>공연축제기획 컨설턴트</li>
+						<li>무대공연음향 사운드디자이너</li>
+						<li>Tone studio Engineer</li>
+						<li>대중음악 작·편곡자</li>
+						<li>컴퓨터 음악관련<br><i>(방송, CF, 영화음악  제작자)</i></li>
+					</ul>
+					<p>산학연계 <span>(주) 글로워스, (주) SP Music, STORY MUSIC</span></p>
+				</div>
+			</section>
+			<section class="subject">
+				<div class="section-header">
+					<hr />
+					<h3>교과과목</h3>
+				</div>
+				<div class="section-content">
+					<ul class="content-list">
+						<li>공연워크숍</li>
+						<li>전공실기</li>
+						<li>레코딩 & 음반프로젝트</li>
+						<li>댄스 퍼포먼스</li>
+						<li>보컬트레이닝</li>
+						<li>보컬 앙상블 & 팝코러스</li>
+						<li>컴퓨터음악</li>
+						<li>미디 시퀀싱</li>
+						<li>디지털 오디오</li>
+						<li>화성학</li>
+						<li>시창 · 청음</li>
+						<li>미디 & 레코딩 테크닉</li>
+						<li>창작 국악가요</li>
+						<li>리듬악기 실습</li>
+						<li>건반악기 실습</li>
+						<li>댄스 트레이닝</li>
+						<li>대중음악론</li>
+						<li>미디어음악론</li>
+					</ul>
+				</div>
 			</section>
 		</div>
 	</main>
-<style>
-.intro {
-	position:relative;
-	background-image:url('/departments/images/intro_bg.jpg');
-	background-size:cover;
-	background-repeat:no-repeat;
-	background-position:50% 50%;
-}
-
-section .section-header {padding:69px 0 42px 0; }
-section .section-header hr { 
-	margin:0 0 12px 1px;
-	width: 24px;
-	border-top-color:#000;
-	text-align:left; 
-}
-
-section .section-header h3 { 
-	margin:0;
-	font-size:30px; 
-	color:#0d0d0d;
-	font-weight:300;
-}
-section .section-content { }
-section .section-content h4 { 
-	font-size:30px;
-	font-weight:300;
-	color:#0d0d0d;
-	margin:0; 
-}
-section .section-content p { 
-	margin-top:50px;
-	font-size:15px; 
-	color:#666;
-	line-height:24px;
-}
-section .section-content p strong { 
-	font-size:15px; 
-	color:#333;
-}
-section .section-content .point { 
-	margin-top:33px;
-}
-section .section-content .contact-office { 
-	width:581px;
-	height:49px;
-	border:1px solid #d6d5d5;
-	background-color:#f6f6f6;
-	padding:13px 17px 13px 26px; 
-	margin:47px 0 70px 0;
-}
-section .section-content .contact-office dl {overflow:hidden; }
-section .section-content .contact-office dl dt { 
-	float:left; 
-	font-size:14px;   
-	color:#555;
-	font-weight:normal;
-	padding:0 8px 0 25px;
-}
-section .section-content .contact-office dl dt:nth-of-type(1) {
-	background-image:url('/departments/images/tel.png');
-	background-size:15px auto;
-	background-repeat:no-repeat;
-	background-position:0 3px;
-}
-section .section-content .contact-office dl dt:nth-of-type(2) {
-	background-image:url('/departments/images/site.png');
-	background-size:17px auto;
-	background-repeat:no-repeat;
-	background-position:0 3px;
-	margin-left:40px;
-}
-section .section-content .contact-office dl dd {
-	position:relative;
-	float:left; 
-	font-size:14px;   
-	color:#555; 
-	padding-left:10px;
-}
-section .section-content .contact-office dl dd:before {position:absolute; top:0; left:0; content:"|";  }
-</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/docfoot.php'); ?>
 </body>
--- departments/sub.php
+++ departments/sub.php
@@ -1,5 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/dochead.php'); ?>
-<link href="/css/introduce.css" rel="stylesheet" />
+<link href="/departments/css/introduce.css" rel="stylesheet" />
 </head>
 <body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/header.php'); ?>
@@ -71,44 +71,18 @@
 				<li>보컬트레이너</li>
 				<li>보컬코러스팀</li>
 				<li>보컬음향디렉터</li>
-
 			</ul>
+			<div class="search-area" style="margin:30px 0;">
+				<form>
+					<div class="form-group">
+						<label for="search-keyword" class="sr-only">검색어</label>
+						<input type="text" name="#" id="search-keyword" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn"><span class="glyphicon glyphicon-search"></span><span class="sr-only">검색</span></button>
+				</form>
+			</div>
 		</div>
 	</main>
-<style>
-/* btn-sm */
-.btn-area {margin:30px 0 30px 0; }
-.btn-area .btn-sm {width:90px; height:30px !important; border-radius:0; font-size:14px; text-decoration:none; color:#fff; text-align:center; padding:0; line-height:28px !important; }
-.btn-area .btn-gray {background-color:#333; }
-.btn-area .btn-pink {background-color:#d22d36; }
-.btn-area .btn-white {background-color:#fff; border:1px solid #ccc; width:60px; color:#666 !important; }
-
-/* btn */
-.btn-area .btn {width:100px; height:40px; border-radius:0; font-size:14px; text-decoration:none; color:#fff; text-align:center; padding:0; line-height:40px; }
-
-/* paging */
-.paging .pagination .active a {border-radius:19px !important; background-color:#333; color:#fff; width:32px; }
-.paging .pagination .active a:hover {background-color:#333 !important; color:#fff; text-decoration:none; }
-.paging .pagination li {padding:0 5px; }
-.paging .pagination a {color:#666; font-size:14px; border-radius:0 !important; border:0; }
-.paging .pagination a:hover {text-decoration:underline; background-color:#fff !important; }
-
-
-/* content-list */
-.content-list {margin-left:10px; }
-.content-list li {
-	background-image:url('/departments/images/r_arrow_l.png');
-	background-size:5px auto;
-	background-repeat:no-repeat;
-	background-position:0 7px;
-	padding-left:22px;
-	width:223px;
-	font-size:15px;
-	color:#666;
-	display:inline-block;
-}
-
-</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/docfoot.php'); ?>
 </body>
