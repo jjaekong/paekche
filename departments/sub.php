056e6f156013d1ca22bc4d56dfefb966e1caf2ce
김재홍 <jjaekong@gmail.com>
2016-06-09 14:27:53 +0900

    서브 수정

--- departments/sub.php
+++ departments/sub.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/dochead.php'); ?>
+<link href="/departments/css/introduce.css" rel="stylesheet" />
 </head>
 <body>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/header.php'); ?>
@@ -28,6 +29,65 @@
     </div>
     <main id="content" tabindex="-1">
         <div class="container">
+            <div class="btn-area">
+				<p>
+					<a class="btn btn-sm btn-gray" href="#" role="button">목록</a>
+					<a class="btn btn-sm btn-white" href="#" role="button">수정</a>
+					<a class="btn btn-sm btn-white" href="#" role="button">삭제</a>
+					<a class="btn btn-sm btn-pink" href="#" role="button">글쓰기</a>
+				</p>
+			</div>
+			<div class="btn-area">
+				<p>
+					<a class="btn btn-gray" href="#" role="button">확인</a>
+					<a class="btn btn-pink" href="#" role="button">취소</a>
+				</p>
+			</div>
+			<nav class="paging">
+				<ul class="pagination">
+					<li>
+						<a href="#" aria-label="first-Previous">
+							<span aria-hidden="true">&laquo;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#" aria-label="Previous">
+							<span aria-hidden="true">&lt;</span>
+						</a>
+					</li>
+					<li class="active"><a href="#">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li><a href="#">4</a></li>
+					<li><a href="#">5</a></li>
+					<li>
+						<a href="#" aria-label="Next" >
+							<span aria-hidden="true">&gt;</span>
+						</a>
+					</li>
+					<li>
+						<a href="#" aria-label="end-Next">
+							<span aria-hidden="true">&raquo;</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<ul class="content-list">
+				<li>대중음악가수</li>
+				<li>레코딩 &amp; 음반프로젝트</li>
+				<li>보컬트레이너</li>
+				<li>보컬코러스팀</li>
+				<li>보컬음향디렉터</li>
+			</ul>
+			<div class="search-area" style="margin:30px 0;">
+				<form>
+					<div class="form-group">
+						<label for="search-keyword" class="sr-only">검색어</label>
+						<input type="text" name="#" id="search-keyword" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn"><span class="glyphicon glyphicon-search"></span><span class="sr-only">검색</span></button>
+				</form>
+			</div>
         </div>
     </main>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/departments/inc/footer.php'); ?>
