957642d8efee9ed58c486d69444d0cf03d6b93ee
김재홍 <jjaekong@gmail.com>
2016-06-18 18:11:56 +0900

    모바일: 레이아웃 작성 중 & 메인페이이지 작성 중

---
+++ mobile/index.php
@@ -0,0 +1,35 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/main.css" rel="stylesheet">
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <main id="content" tabindex="-1">
+        <div class="campaigns">
+            <ul>
+                <li>
+                    <h3>
+                        백제 단편 시나리오<br>
+                        공모전 당선자 발표
+                    </h3>
+                    <p>재학생 여러분들의 많은 관심과 참여로 진행된 '백제 단편 시나리오 공모전' 당선자를 발표합니다.</p>
+                    <a href="#">자세히 보기</a>
+                </li>
+            </ul>
+        </div>
+        <div class="shortcuts">
+            <div class="container">
+                <ul class="row">
+                    <li class="col-xs-4"><a href="#">대학소개</a></li>
+                    <li class="col-xs-4"><a href="#">입학안내</a></li>
+                    <li class="col-xs-4"><a href="#">학사일정</a></li>
+                    <li class="col-xs-4"><a href="#">학사공지</a></li>
+                    <li class="col-xs-4"><a href="#">NEWS</a></li>
+                    <li class="col-xs-4"><a href="#">오시는 길</a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
