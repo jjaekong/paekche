16c74c5e030a428b9a66e21cf47f4b2e8ea32251
kyh5392 <kyh90495392@gmail.com>
2016-06-20 19:25:42 +0900

    [모바일] 대학소개 > 설립이념 #1, 비전/발전전략 (작업중) #1

---
+++ mobile/introduce/ideology.php
@@ -0,0 +1,64 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/sub.css" rel="stylesheet">
+<link href="/mobile/css/introduce.css" rel="stylesheet">
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+	<main id="content" tabindex="-1" class="introduce ideology">
+		<div class="container">
+			<div class="page-header">
+				<hr />
+				<h2>설립이념</h2>
+				<p>우리 대학의 설립과 <i>교육 목표 체계</i></p>
+			</div>
+			<section class="tenontology">
+				<div class="section-header">
+					<h3>건학이념</h3>
+				</div>
+				<div class="section-content">
+					<p>우리문화예술의 정통성을 바탕으로<br> 그 본류를 주도하고 미래를 선도하는<br><strong>"예술인 교육"</strong></p>
+					<img src="/mobile/images/introduce/intro_img_1.gif" alt="자연인,문화인,예술인" class="img-responsive">
+				</div>
+			</section>
+			<section class="purpose">
+				<div class="section-header">
+					<h3>교육목적</h3>
+				</div>
+				<div class="section-content">
+					<strong>"우리문화예술의 정통성을 바탕으로 그 본류를 주도하고 미래를 선도하는 예술인 교육"</strong>
+					<p>이라는 건학 이념을 구현하기 위해 우리는 우리문화예술의 본질을 이해하고 존중하며, 사회와 같이 호흡하고 고민하는 예술가 정신을 함양하여 각자의 예술표현의 욕구를 실제 예술현장에서 살아 있는 모습으로 행위 함으로써, 당대 문화예술의 본류를 이루고 또 이끌어가는 예술인, 예능인으로 교육하고자 한다.</p>
+				</div>
+			</section>
+			<section class="target">
+				<div class="section-header">
+					<h3>교육목표</h3>
+				</div>
+				<div class="section-content">
+					<strong>우리 백제예술대학교는 다음과 같은 예술인상을 길러낸다.</strong>
+					<ul class="row">
+						<li class="col-xs-6 col-sm-3">
+							<img src="/mobile/images/introduce/target_img1.png" alt="사회와 대중과 같이 호흡하는 예술인" class="img-responsive">
+							<p>사회와 대중과 같이<br>호흡하는 예술인</p>
+						</li>
+						<li class="col-xs-6 col-sm-3">
+							<img src="/mobile/images/introduce/target_img2.png" alt="창의적인 예술인" class="img-responsive">
+							<p>창의적인<br>예술인</p>
+						</li>
+						<li class="clearfix visible-xs-block" role="presentation"></li>
+						<li class="col-xs-6 col-sm-3">
+							<img src="/mobile/images/introduce/target_img3.png" alt="행동하고 고민하는 예술인" class="img-responsive">
+							<p>행동하고 고민하는<br>예술인</p>
+						</li>
+						<li class="col-xs-6 col-sm-3">
+					 		<img src="/mobile/images/introduce/target_img4.png" alt="시대적 요구와 조화되는 예술인" class="img-responsive">
+							<p>시대적 요구와 조화되는<br>예술인</p>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ mobile/sub.php
@@ -0,0 +1,142 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/sub.css" rel="stylesheet">
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+	<main id="content" tabindex="-1" class="sub">
+		<div class="container">
+			<div class="page-header">
+				<hr />
+				<h2>설립이념</h2>
+				<p>우리 대학의 설립과 <i>교육 목표 체계</i></p>
+			</div>
+			<section>
+				<div class="section-header">
+					<h3>건학이념</h3>
+				</div>
+
+				<ul class="standard-list" style="margin:20px 0;">
+					<li>내용</li>
+					<li>내용</li>
+					<li>내용</li>
+				</ul>
+
+				<ul class="arrow-list" style="margin:20px 0;">
+					<li>내용</li>
+					<li>내용</li>
+					<li>내용</li>
+				</ul>
+				
+				<div class="table-wrap">
+					<table class="table table-border">
+						<colgroup>
+							<col style="width: /">
+							<col style="width: /">
+							<col style="width: /">
+							<col style="width: /">
+						</colgroup>
+						<thead>
+							<tr>
+								<th>제목</th>
+								<th>제목</th>
+								<th>제목</th>
+								<th>제목</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+							</tr>
+							<tr>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+
+				<div class="table-wrap" style="margin:30px 0;">
+					<table class="table table-border">
+						<colgroup>
+							<col style="width: /">
+							<col style="width: /">
+							<col style="width: /">
+							<col style="width: /">
+						</colgroup>
+						<tbody>
+							<tr>
+								<th>내용</th>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+							</tr>
+							<tr>
+								<th>내용</th>
+								<td>내용</td>
+								<td>내용</td>
+								<td>내용</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+
+				<nav class="paging">
+					<ul class="pagination">
+						<li>
+							<a href="#" aria-label="first-Previous">
+								<span aria-hidden="true">&laquo;</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" aria-label="Previous">
+								<span aria-hidden="true">&lsaquo;</span>
+							</a>
+						</li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li>
+							<a href="#" aria-label="Next" >
+								<span aria-hidden="true">&rsaquo;</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" aria-label="end-Next">
+								<span aria-hidden="true">&raquo;</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+
+				<div class="search-area" style="margin:30px 0;">
+					<form>
+						<div class="form-group">
+							<label for="search-keyword" class="sr-only">검색어</label>
+							<input type="text" name="#" id="search-keyword" class="form-control" placeholder="Search">
+						</div>
+						<button type="submit" class="btn"><span class="glyphicon glyphicon-search"></span><span class="sr-only">검색</span></button>
+					</form>
+				</div>
+				
+				<div class="btn-area">
+					<p>
+						<a href="#" class="btn gray-btn" role="button">목록</a>
+						<a href="#" class="btn white-btn" role="button">취소</a>
+						<a href="#" class="btn pink-btn" role="button">확인</a>
+						<button class="btn pink-btn" type="submit">submit</button>
+					</p>
+				</div>
+			</section>
+		</div>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
