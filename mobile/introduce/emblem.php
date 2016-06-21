a15496152fa27297a896e3ba8fee57bb1d8bd929
kyh5392 <kyh90495392@gmail.com>
2016-06-21 12:27:14 +0900

    [모바일] 대학소개 > 특성화전략 #1 , 상징소개 #1 완료.

---
+++ mobile/introduce/emblem.php
@@ -0,0 +1,66 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/sub.css" rel="stylesheet">
+<link href="/mobile/css/introduce.css" rel="stylesheet">
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+	<main id="content" tabindex="-1" class="introduce emblem">
+		<div class="container">
+			<div class="page-header">
+				<hr />
+				<h2>상징소개</h2>
+				<p>우리 대학의 <i>교육 목표 체계</i></p>
+			</div>
+			<section class="didactic">
+				<div class="section-header">
+					<h3>교훈</h3>
+				</div>
+				<div class="section-content">
+					<h4>PAEKCHE<br><small>INSTITUTE OF THE ARTS</small></h4>
+					<img src="/mobile/images/introduce/emblem_img.png" class="img-responsive" alt="교훈석">
+					<img src="/mobile/images/introduce/emblem_img2.gif" class="img-responsive" alt="자연인,문화인,예술인">
+				</div>
+			</section>
+			<section class="logo-symbol">
+				<div class="section-header">
+					<h3>교표</h3>
+				</div>
+				<div class="section-content">
+					<h4>Logo</h4>
+					<img src="/mobile/images/introduce/logo.gif" class="img-responsive" alt="로고">
+					<h4>Symbol</h4>
+					<img src="/mobile/images/introduce/symbol.gif" class="img-responsive" alt="상징">
+					<h5>백제예술대학교 로고는</h5>
+					<p><i>Cosmos(우주)</i>와 <i>Global(세계적인)</i>을 상징하는 2개의 원 속에 창의적 감성(Yellow)과 냉철한 이성(Balck)이 조화롭게 이루어져 화려하게 빛나는<i>신성(new star)</i>을 중심으로 역동적 에너지의 불꽃으로 승화하는 이미지를 상징한다.</p>
+				</div>
+			</section>
+			<section class="school-banner">
+				<div class="section-header">
+					<h3>교기</h3>
+					<p>백제예술대학교 <i>교기</i></p>
+				</div>
+				<div class="section-content">
+					<img src="/mobile/images/introduce/school_banner.png" class="img-responsive" alt="교기">
+					<p>교기</p>
+				</div>
+			</section>
+			<section class="school-song">
+				<div class="section-header">
+					<h3>교가</h3>
+					<p>백제예술대학교 <i>교가</i></p>
+				</div>
+				<div class="section-content">
+					<div class="btn-area">
+						<p>
+							<a href="#" class="btn song-btn">교가 듣기</a>
+							<a href="#" class="btn sheet-btn">악보 보기</a>
+						</p>
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
