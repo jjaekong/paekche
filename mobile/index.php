651af2820bb801ba16e639f5aa152e62c411f22a
김재홍 <jjaekong@gmail.com>
2016-06-22 19:13:06 +0900

    모바일: 메인페이지 컨테이너(#container) 제거, 비주얼 이미지 가운데 정렬

--- mobile/index.php
+++ mobile/index.php
@@ -2,31 +2,29 @@
 <link href="/mobile/css/main.css" rel="stylesheet">
 </head>
 <body>
-    <div id="container">
-        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
-        <main id="content" tabindex="-1">
-            <div class="campaigns">
-                <ul>
-                    <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
-                    <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
-                    <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <main id="content" tabindex="-1">
+        <div class="campaigns">
+            <ul>
+                <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
+                <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
+                <li><a href="#"><img class="img-responsive" src="/mobile/images/main/img_campaign_1.jpg" alt=""></a></li>
+            </ul>
+        </div>
+        <div class="shortcuts">
+            <div class="container">
+                <ul class="row">
+                    <li class="col-xs-4"><a href="#">대학소개</a></li>
+                    <li class="col-xs-4"><a href="#">입학안내</a></li>
+                    <li class="col-xs-4"><a href="#">학사일정</a></li>
+                    <li class="col-xs-4"><a href="#">학사공지</a></li>
+                    <li class="col-xs-4"><a href="#">NEWS</a></li>
+                    <li class="col-xs-4"><a href="#">오시는 길</a></li>
                 </ul>
             </div>
-            <div class="shortcuts">
-                <div class="container">
-                    <ul class="row">
-                        <li class="col-xs-4"><a href="#">대학소개</a></li>
-                        <li class="col-xs-4"><a href="#">입학안내</a></li>
-                        <li class="col-xs-4"><a href="#">학사일정</a></li>
-                        <li class="col-xs-4"><a href="#">학사공지</a></li>
-                        <li class="col-xs-4"><a href="#">NEWS</a></li>
-                        <li class="col-xs-4"><a href="#">오시는 길</a></li>
-                    </ul>
-                </div>
-            </div>
-        </main>
-        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
-    </div>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
     <script src="/js/jquery.bxslider.min.js"></script>
     <script>
