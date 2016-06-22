c651970da7548585c62acf90ff535a55bf8335ba
kyh5392 <kyh90495392@gmail.com>
2016-06-22 18:18:31 +0900

    [모바일] 대학소개 > 오시는길 #2 img 위치 수정 , 백제광장 > 뉴스&공지사항 (board_write.php) #2 <textarea> border-color 수정 ,

--- mobile/community/board_write.php
+++ mobile/community/board_write.php
@@ -4,110 +4,52 @@
 </head>
 <body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
-	<main id="content" tabindex="-1" class="community schedule">
+	<main id="content" tabindex="-1" class="community board-write">
 		<div class="container">
 			<div class="page-header">
 				<hr />
-				<h2>학사일정</h2>
-				<p>백제예술대학교 <i>학사일정</i></p>
+				<h2>뉴스 & 공지사항</h2>
+				<p>백제예술대학교 <i>뉴스 & 공지사항</i></p>
 			</div>
-			<div class="calendar">
-				<article class="article-header">
-					<h3>
-						06<br>
-						<small>JUNE</small>
-					</h3>
-					<a href="#" class="btn left-btn"><img src="/mobile/images/community/left_btn.png" alt="이전"></a>
-					<a href="#" class="btn right-btn"><img src="/mobile/images/community/right_btn.png" alt="다음"></a>
-				</article>
-				<article class="article-content">
-					<div class="calendar-table">
-						<table class="table">
+			<div class="board-content">
+				<form>
+					<div class="table-wrap">
+						<table class="table table-border">
 							<colgroup>
-								<col style="wdith:40px;"/>
-								<col />
-								<col />
-								<col />
-								<col />
-								<col />
-								<col />
+								<col style="width:65px;"/>
+								<col style="width:225px;"/>
 							</colgroup>
 							<tbody>
-								<tr>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td>1</td>
-									<td>2</td>
-									<td>3</td>
-									<td>4</td>
+								<tr class="writer">
+									<th><label for="user-name">작성자</label></th>
+									<td><input type="text" id="user-name" name="#"></td>
 								</tr>
-								<tr>
-									<td>5</td>
-									<td><i>6</i></td>
-									<td><i>7</i></td>
-									<td><i>8</i></td>
-									<td><i>9</i></td>
-									<td><i>10</i></td>
-									<td>11</td>
+								<tr class="subject">
+									<th><label for="subject">제목</label></th>
+									<td><input type="text" id="subject" name="#"></td>
 								</tr>
-								<tr>
-									<td>12</td>
-									<td><i>13</i></td>
-									<td><i>14</i></td>
-									<td><i>15</i></td>
-									<td><i>16</i></td>
-									<td><i>17</i></td>
-									<td>18</td>
+								<tr class="text-content">
+									<th><label for="text-content">내용</label></th>
+									<td><textarea id="text-content" name="#"></textarea></td>
 								</tr>
-								<tr>
-									<td>19</td>
-									<td>20</td>
-									<td>21</td>
-									<td>22</td>
-									<td><i>23</i></td>
-									<td><i>24</i></td>
-									<td><i>25</i></td>
+								<tr class="file">
+									<th><label for="file" class="attach">첨부파일</label></th>
+									<td><input type="file" id="file" name="#"></td>
 								</tr>
-								<tr>
-									<td><i>26</i></td>
-									<td><i>27</i></td>
-									<td><i>28</i></td>
-									<td><i>29</i></td>
-									<td>30</td>
-									<td>31</td>
-									<td></td>
+								<tr class="password">
+									<th><label for="password">비밀번호</label></th>
+									<td><input type="password" id="password" name="#"></td>
 								</tr>
 							</tbody>
 						</table>
 					</div>
-				</article>
-				<article class="article-footer">
-					<dl>
-						<dt>06일</dt>
-						<dd>현충일</dd>
-					</dl>
-					<dl>
-						<dt>07일 ~ 10일</dt>
-						<dd>1학기 기말고사</dd>
-					</dl>
-					<dl>
-						<dt>10일</dt>
-						<dd>종강</dd>
-					</dl>
-					<dl>
-						<dt>13일 ~ 17일</dt>
-						<dd>보강주 / 성적입력기간</dd>
-					</dl>
-					<dl>
-						<dt>23일 ~ 28일</dt>
-						<dd>성적확인 및 정정기간</dd>
-					</dl>
-					<dl>
-						<dt>29일</dt>
-						<dd>출석부 및 성적단표 제출</dd>
-					</dl>
-				</article>
+					<div class="btn-area">
+						<p>
+							<a href="#" class="btn white-btn" role="button">취소</a>
+							<button href="#" class="btn pink-btn" type="submit">완료</>
+						</p>
+					</div>
+				</form>
 			</div>
 		</div>
 	</main>
