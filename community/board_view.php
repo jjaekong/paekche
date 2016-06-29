793a467e0471c451fee41200e79356c87678ab3d
kyh5392 <kyh90495392@gmail.com>
2016-06-29 12:56:05 +0900

    백제광장 > 뉴스&공지사항 리플 및 댓글달기 추가 작업 완료.#1

--- community/board_view.php
+++ community/board_view.php
@@ -64,6 +64,9 @@
                         <div class="board-content">
                             제가 온라인으로 복학신청을 했는데요 ~저장을 누르면 끝나는건가요?신청되었는지 궁금합니다!
                         </div>
+						<ul class="attach">
+							<li><a href="#">20160413789456.jpg </a></li>
+						</ul>
                         <ul class="board-nav">
                             <li class="next"><a href="#"><span class="glyphicon glyphicon-menu-up"></span>온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제 온라인 복학신청문제</a></li>
                             <li class="prev"><a href="#"><span class="glyphicon glyphicon-menu-down"></span>온라인 복학신청합니다~</a></li>
@@ -81,10 +84,202 @@
                             </div>
                         </div>
                     </article>
+					<div class="reply-area">
+						<!--
+						<p class="no-reply">등록된 댓글이 없습니다.</p>
+						-->
+						<article class="reply">
+							<div class="reply-header">
+								<p>학생지원처 <span>2016.01.20</span></p>
+								<ul>
+									<li><a href="#" class="modify">수정</a></li>
+									<li><a href="#" class="delete">삭제</a></li>
+								</ul>
+							</div>
+							<div class="reply-content">
+								<p>
+									안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 
+								</p>
+							</div>
+						</article>
+						<article class="reply">
+							<div class="reply-header">
+								<p>학생지원처 <span>2016.01.20</span></p>
+								<ul>
+									<li><a href="#" class="modify">수정</a></li>
+									<li><a href="#" class="delete">삭제</a></li>
+								</ul>
+							</div>
+							<div class="reply-content">
+								<p>
+									안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 
+								</p>
+							</div>
+						</article>
+						<article class="reply">
+							<div class="reply-header">
+								<p>학생지원처 <span>2016.01.20</span></p>
+								<ul>
+									<li><a href="#" class="modify">수정</a></li>
+									<li><a href="#" class="delete">삭제</a></li>
+								</ul>
+							</div>
+							<div class="reply-content">
+								<p>
+									안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 
+								</p>
+							</div>
+						</article>
+						<article class="reply">
+							<div class="reply-header">
+								<p>학생지원처 <span>2016.01.20</span></p>
+								<ul>
+									<li><a href="#" class="modify">수정</a></li>
+									<li><a href="#" class="delete">삭제</a></li>
+								</ul>
+							</div>
+							<div class="reply-content">
+								<p>
+									안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 안녕하세요 학생지원처입니다. 
+								</p>
+							</div>
+						</article>
+						<div class="reply-form">
+							<form>
+								<textarea name="#" id="re-write"></textarea>
+								<button style="submit">등록</button>
+							</form>
+						</div>
+					</div>
                 </main>
             </div>
         </div>
     </div>
+<style>
+.community.view {
+	padding-bottom:150px;
+}
+.community.view .board-view .attach {
+	padding:0 20px 20px;
+	background-image:url(/images/ico_clip.png);
+	background-repeat:no-repeat;
+	background-position:20px 4px;
+	border-bottom:1px solid #ccc;
+}
+.community.view .board-view .attach li {
+	display:inline-block;
+	margin-right:5px;
+}
+.community.view .board-view .attach li:nth-of-type(1) {
+	padding-left:20px;
+}
+.community.view .board-view .attach li:after {
+	content:",";
+}
+.community.view .board-view .attach li:last-child {
+	margin-right:0;
+}
+.community.view .board-view .attach li:last-child:after {
+	content:"";
+}
+
+
+
+
+
+.community.view .board-view .attach li a {
+	color:#999;
+	font-size:14px;
+}
+.community.view .reply-area {
+	margin-top:40px;
+}
+.community.view .reply-area .reply .reply-header {
+	overflow:hidden;
+	background-color:#f9f9f9;
+	padding:10px 30px 10px 30px;
+	border-top:1px solid #ccc;
+}
+.community.view .reply-area .reply .reply-header > p {
+	font-size:14px;
+	font-weight:500;
+	display:inline-block;
+}
+.community.view .reply-area .reply .reply-header > p span {
+	font-size:12px;
+	font-weight:300;
+}
+.community.view .reply-area .reply .reply-header > p span:before {
+	content:"|";
+	padding:10px;
+}
+.community.view .reply-area .reply .reply-header ul {
+	float:right;
+}
+.community.view .reply-area .reply .reply-header ul li {
+	display:inline-block;
+	font-size:14px;
+}
+.community.view .reply-area .reply .reply-header ul li:last-child {
+	margin-left:20px;
+}
+.community.view .reply-area .reply .reply-header ul li a {
+	color:#999;
+	padding-left:15px;
+}
+.community.view .reply-area .reply .reply-header ul li .modify {
+	background-image:url(/images/community/btn_modify.gif);
+	background-repeat:no-repeat;
+	background-position:0 5px;
+}
+.community.view .reply-area .reply .reply-header ul li .delete {
+	background-image:url(/images/community/btn_del.gif);
+	background-repeat:no-repeat;
+	background-position:0 5px;
+}
+.community.view .reply-area .reply .reply-content {
+	padding:20px;
+	font-size:14px;
+	line-height:30px;
+}
+.community.view .reply-area .reply-form {
+	margin-top:30px;
+	border-top:1px solid #ccc;
+}
+.community.view .reply-area .reply-form form {
+	margin-top:30px;
+	padding:20px;
+	border:1px solid #ccc;
+	background-color:#f7f7f7;
+	overflow:hidden;
+}
+.community.view .reply-area .reply-form form textarea {
+	width:705px; 
+	height:100px;
+	float:left;
+	border-color:#ebebeb;
+}
+.community.view .reply-area .reply-form form button {
+	color:#fff;
+	width:100px;
+	height:100px;
+	border:0;
+	margin-left:20px;
+	background-color:#b2b2b2;
+	float:left;
+	font-size:14px;
+}
+
+
+
+.community.view .reply-area .no-reply { 
+	font-size:14px;
+	border-top:1px solid #ccc;
+	padding:20px;
+	text-align:center;
+}
+
+</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
 </body>
